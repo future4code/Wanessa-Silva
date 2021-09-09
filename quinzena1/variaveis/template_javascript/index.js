b2e3af994bcb234ed8cabe5f9b770a3e0fba7e1c
Wanessa Santos <wannsanttos@gmail.com>
2021-09-08 23:21:42 -0300

    Exercício completo de JS

---
+++ quinzena1/variaveis/template_javascript/index.js
@@ -0,0 +1,67 @@
+/* Questão 1
+10
+10, 10 */
+
+/* Questão 2
+10
+20
+Undefined */
+
+/*Questão 3
+let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let ganhoPorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasTrabalhadas/ganhoPorDia} por hora`)*/
+
+//const nome
+
+//let idade
+
+//console.log (typeof nome)
+
+//console.log (typeof idade)
+
+/* A resposta tanto de nome como de idade foi Undefined, 
+pois não foi atribuido nenhum valor para elas. */
+
+//const nome = prompt ("Digite seu nome")
+//let idade = prompt ("Digite sua idade")
+
+//const nome = "Wanessa"
+//let idade = "27"
+
+//console.log (typeof nome)
+//console.log (typeof idade)
+
+/* As duas são Strings, pois foi atribuido um valor para elas, 
+e apesar de idade parecer um Number, qualquer resultado de um prompt é uma string */
+
+//console.log ("Olá", nome, ", você tem", idade , "anos.")
+
+//let agua = prompt ("Você bebeu muita água hoje?")
+//let sono = prompt ("Você está com sono agora?")
+//et fome = prompt ("Você sente fome agora?")
+
+//let agua = "Não"
+//let sono = "Sim"
+//let fome = "Não"
+
+//console.log ("Você bebeu muita água hoje? - Não")
+//console.log ("Você está com sono agora? - Sim")
+//console.log ("Você sente fome agora? - Não")
+
+//let a = 10
+//let b = 25
+//let c = a
+//a = b
+//b = c
+//console.log("O valor de a é:", a, "O valor de b é:", b)
+
+
+
+
+
+
+
+
+
+
