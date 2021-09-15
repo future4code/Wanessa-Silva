e7cb71e1e2a70c38e8ad332cf4468bf26fabe8b9
Wanessa Santos <wannsanttos@gmail.com>
2021-09-14 23:31:23 -0300

    exercício completo aula5

---
+++ quinzena1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,56 @@
+/* =========================== Exercícios de interpretação de código =========================== */
+// Exercício 1
+/* a.undefined
+   b.null
+   c.  11
+   d.  3
+   e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13 ]
+   f.  9 */
+
+// Exercício 2
+// SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+/* =============================== Exercícios de escrita de código =============================== */
+// Exercício 1
+/* const nomeDoUsuario = prompt('Digite seu nome'); 
+const emailDoUsuario = prompt('Digite seu email');
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`); */
+
+/* ========================================== Exercício 02 ========================================== */
+
+/* const comidas = ['pizza', 'lasanha', 'macarrao', 'sushi', 'açai'];
+
+console.log(comidas);
+
+console.log(`Essas são as minhas comidas preferidas:\n ${comidas[0]}\n ${comidas[1]}\n ${comidas[2]}\n ${comidas[3]}\n ${comidas[4]}\n`);
+
+const novaComida = prompt('Digite uma comida');
+comidas[1] = novaComida; 
+console.log(`Essas é a nova lista de comidas preferidas:\n ${comidas[0]}\n ${comidas[1]}\n ${comidas[2]}\n ${comidas[3]}\n ${comidas[4]}\n`); */
+
+/* ========================================== Exercício 03 ========================================== */
+/* 
+const listaDeTarefas = [];
+
+listaDeTarefas[0] = prompt('Digite uma tarefa'); 
+listaDeTarefas[1] = prompt('Digite uma tarefa'); 
+listaDeTarefas[2] = prompt('Digite uma tarefa'); 
+
+console.log(listaDeTarefas); 
+
+const posicao = prompt('Digite o índice da tarefa que você já realizou: 0, 1 ou 2'); 
+
+listaDeTarefas.splice(posicao, 1);
+
+console.log(listaDeTarefas); */
+
+
+/* ============================================ Desafio ============================================ */
+// 01
+/* const frase = prompt('Digite uma frase');
+console.log(frase.split(' ')); */
+
+
+// 02
+/* const array = ['Banana', 'Morango', 'Abacaxi', 'Laranja', 'Ameixa'];
+console.log("O índice da palavra Abacaxi é:", array.indexOf('Abacaxi'), "O tamanho do array é:", array.length); */
