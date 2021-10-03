1de575ce5d93ffd4306f2552bdab1fe792f1bc4b
Wanessa Santos <wannsanttos@gmail.com>
2021-10-03 18:58:44 -0300

    Ajustes do projeto blackjack

--- quinzena2/blackjack/nossoJogo.js
+++ quinzena2/blackjack/nossoJogo.js
@@ -18,11 +18,24 @@ if(confirm('Quer iniciar uma nova rodada?')) {
 	console.log('O jogo acabou!')
 }
 
-const carta = comprarCarta(); 
-// Sorteia uma carta. Por exemplo, o rei de ouros
+const carta1Usuario = comprarCarta()
+const carta2Usuario = comprarCarta()
+const carta1Computador = comprarCarta()
+const carta2Computador = comprarCarta()
 
-console.log(carta.texto) 
-// imprime o texto da carta. Nesse caso: "K♦️"
+const somaUsuario = (carta1Usuario.texto + carta2Usuario.texto)
+const somaComputador = (carta1Computador.texto + carta2Computador.texto)
 
-console.log(carta.valor) 
-// imprime o valor da carta (um número). Nesse caso: 10)
+const somaUsuario1 = carta1Usuario.valor + carta2Usuario.valor
+const somaComputador1 = carta1Computador.valor + carta2Computador.valor
+
+console.log("Usúario - Cartas:" + somaUsuario + " Pontuação:" + somaUsuario1)
+console.log("Computador - Cartas:" + somaComputador + " Pontuação:" + somaComputador1)
+
+if (somaUsuario1 > somaComputador1){
+   console.log('Você ganhou!')
+}else if(somaComputador1 > somaUsuario1){
+   console.log('Você perdeu!')
+}else if (somaUsuario1 === somaComputador1){
+   console.log('Vocês Empataram')
+}
