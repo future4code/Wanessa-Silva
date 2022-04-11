3b597f5ec86d1ee73cb7f1a5d9bfa30e3acf3cf4
Wanessa Santos <wannsanttos@gmail.com>
2022-04-10 22:09:51 -0300

    continuação sistema bancario

---
+++ modulo06/sistema-bancario/src/dataBase.ts
@@ -0,0 +1,18 @@
+
+
+export type Users = {
+    name: string,
+    CPF: string,
+    birthday: string,
+    balance: number,
+   
+}
+
+export const users: Users[] = [
+    {
+        name: "João",
+        CPF: "555.666.777-88",
+        birthday: "31/03/1996",
+        balance: 0,
+    }
+]
